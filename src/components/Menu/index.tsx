3f84dc4c5ee741e7d26dbd383519f385cc723388
basisesports <72276743+basisesports@users.noreply.github.com>
2021-03-08 11:24:12 -0600

    Error fix

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -110,11 +110,11 @@ export default function Menu() {
           {/* <MenuItem id="link" href="https://discord.gg/AgJsrK7">
             <MessageCircle size={14} />
             {t('discord')}
-          </MenuItem>
+          </MenuItem> */}
           <MenuItem id="link" href="https://github.com/jiro-ono/sushiswap-classic-interface">
             <Code size={14} />
             {t('code')}
-          </MenuItem> */}
+          </MenuItem>
         </MenuFlyout>
       )}
     </StyledMenu>
